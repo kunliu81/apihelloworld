88950de86fb75afd1ff5880f00856ae255bf6e7d
Kun <nouse>
2018-01-15 21:41:48 +0100

    ome changes

---
+++ src/test/java/com/solidifi/example/helloworld/apihelloworld/controller/HelloWorldControllerImplTest.java
@@ -0,0 +1,19 @@
+package com.solidifi.example.helloworld.apihelloworld.controller;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloWorldControllerImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
